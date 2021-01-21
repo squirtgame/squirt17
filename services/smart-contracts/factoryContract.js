87dee221c8c9bc0920cb6f0731af418542209b8e
hpb17 <77406633+hpb17@users.noreply.github.com>
2021-01-21 22:28:53 +0000

    Add files via upload

---
+++ services/smart-contracts/factoryContract.js
@@ -0,0 +1,38 @@
+import Contract from "./Contract";
+import {masterContract} from "../../settings";
+import {factoryAbi} from "./factoryAbi";
+
+class FactoryContract extends Contract {
+    constructor(){
+        super(factoryAbi, masterContract);
+    }
+
+    fetch = async () => {
+        return Promise.all([
+            this.getAddress(),
+            this.getBalance(),
+            this.getCount(),
+            this.getLatestSpawnedContract(),
+            this.getPreviousContract(),
+            this.getAdmin(),
+        ]).then(this.arrayToObject);
+    };
+
+    getLatestSpawnedContract = async () => {
+        return this.methods.getLatestSpawnedContract().call()
+            .then(latestSpawnedContract => ({latestSpawnedContract}));
+    };
+
+    getPreviousContract = async () => {
+        return this.methods.previousContract().call()
+            .then(previousContract => ({previousContract}))
+            .catch(() => null);
+    };
+
+    getCount = async () => {
+        return this.methods.getContractCount().call()
+            .then(count => ({count: parseInt(count, 10)}));
+    };
+}
+
+export default new FactoryContract();
