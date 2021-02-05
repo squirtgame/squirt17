1738f6222919cd8818f9ca7cbc7370565e726cff
hpb17 <77406633+hpb17@users.noreply.github.com>
2021-02-05 08:41:36 +0000

    Update SocialMenu.js

--- site-components/menus/SocialMenu.js
+++ site-components/menus/SocialMenu.js
@@ -5,9 +5,12 @@ import {Github, Medium, Reddit, Telegram, Twitter} from "../../components/icons"
 export const SocialMenu = ({className = '', ...props}) => {
     return (
         <nav className={joinClassNames("social-menu round", className)} {...props}>
-            <a target="_blank" href="https://github.com/hpb17"><Github/></a>
+            <a target="_blank" href="https://t.me/hpb17bot"><Telegram/></a>
+            <a target="_blank" href="https://github.com/hpb17/hpb17/"><Github/></a>
             <a target="_blank" href="https://twitter.com/hpb17"><Twitter/></a>
-            <a target="_blank" href="https://reddit.com/user/hpb17"><Reddit/></a>
+            <a target="_blank" href="https://www.reddit.com/r/hpb17/"><Reddit/></a>
+            <a target="_blank" href="https://waxlyrical.medium.com/hpb17-the-hottest-new-fomo-game-for-blockchain-8d4298d04736"><Medium/></a>
         </nav>
     );
 };
+
